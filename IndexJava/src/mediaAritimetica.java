cd0978b7818520aea51043ce0c99b5ea80bdc7c4
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-03-08 19:18:01 -0300

    first commit

---
+++ IndexJava/src/mediaAritimetica.java
@@ -0,0 +1,9 @@
+
+// Exercicio: Média Aritimética
+
+//Instruções: Dadas 3 notas do aluno, imprima a sua média aritimética
+public class mediaAritimetica {
+    public static void main(String[] args) {
+        
+    }
+}
