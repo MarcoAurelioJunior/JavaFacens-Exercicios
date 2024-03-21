9215df872396dfbfe6b89bdd1c0e97d530b39c8a
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-03-21 19:36:54 -0300

    comments

--- IndexJava/src/exercicioCursoExtra/investigacaoCriminosa.java
+++ IndexJava/src/exercicioCursoExtra/investigacaoCriminosa.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 
 public class investigacaoCriminosa {
     public static void main(String[] args) {
-        int numSim = interrogatorio();
-        classificaInterrogado(numSim);
+        int numSim = interrogatorio(); //Cria a variavel numSim atribuindo a função interrogatorio() que está retornando os resultados!
+        classificaInterrogado(numSim); //Chama a funçao classificaInterrogado() e atribui a variavel
     }
 
-    public static void classificaInterrogado(int numSim){
-        if(numSim == 0 || numSim == 1){
+    public static void classificaInterrogado(int numSim) { // Cria a função e coloca como parametro a variavel
+        //Classificação dos resultados!
+        
+        if(numSim == 0 || numSim == 1){ 
             System.out.println("Inocente");
         }else if(numSim == 2){
             System.out.println("Suspeito");
@@ -19,41 +21,42 @@ public class investigacaoCriminosa {
         }
     }
 
-    public static int interrogatorio() {
-        Scanner scanner = new Scanner(System.in);
+    public static int interrogatorio() { //Cria a função interrogatorio()
 
-            int numSim = 0;
+        Scanner scanner = new Scanner(System.in); //Novo Scanner
+
+            int numSim = 0; //Variavel valor inicial 0 
 
             System.out.print("Telefonou para a vítima? ");
             int valor1 = scanner.nextInt();
-                if (valor1 == 1) {
-                    numSim += 1;
+                if (valor1 == 1) { //Se o valor for igual a 1 (Sim) então...
+                    numSim += 1; //Atribua a variavel + 1
                 }
 
             System.out.print("Esteve no local do crime? ");
             int valor2 = scanner.nextInt();
-                if (valor2 == 1) {
-                    numSim += 1;
+                if (valor2 == 1) { //Se o valor for igual a 1 (Sim) então...
+                    numSim += 1; //Atribua a variavel + 1
                 }
 
             System.out.print("Mora perto da vítima? ");
             int valor3 = scanner.nextInt();
-                if (valor3 == 1) {
-                    numSim += 1;
+                if (valor3 == 1) { //Se o valor for igual a 1 (Sim) então...
+                    numSim += 1; //Atribua a variavel + 1
                 }
 
             System.out.print("Devia para a vítima? ");
             int valor4 = scanner.nextInt();
-                if (valor4 == 1) {
-                    numSim += 1;
+                if (valor4 == 1) { //Se o valor for igual a 1 (Sim) então...
+                    numSim += 1; //Atribua a variavel + 1
                 }
 
             System.out.print("Já trabalhou com a vítima? ");
             int valor5 = scanner.nextInt();
-                if (valor5 == 1) {
-                    numSim += 1;
+                if (valor5 == 1) { //Se o valor for igual a 1 (Sim) então...
+                    numSim += 1; //Atribua a variavel + 1
                 }
         scanner.close();      
-        return numSim;
+        return numSim; //Retorna a variavel com os seus devidos resultados
     }
 }
