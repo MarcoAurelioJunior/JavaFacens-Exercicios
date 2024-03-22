8c5939d6b3f4fe342cc2ca3255595f78ae813bb6
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-03-22 17:36:57 -0300

    UPDATE

--- IndexJava/src/exercicioCursoExtra/funcoes/functionNotes.java
+++ IndexJava/src/exercicioCursoExtra/funcoes/functionNotes.java
@@ -19,14 +19,14 @@ public class functionNotes {
             System.out.print("Digite a nota 3: ");
             int valorNum3 = scanner.nextInt();
 
-            System.out.println(calculoMedia(valorNum1, valorNum2, valorNum3));
+            System.out.println(calculoMedia(valorNum1, valorNum2, valorNum3)); //chama a função e coloca os parametros dos dígitos do usuário
         scanner.close();
     }
     public static String comprarSorvete(float dinheiro){ //Cria a função do tipo String
         return "Comprei o sorvete e custou R$ " + dinheiro; //Retorna o texto + a quantidade de dinheiro estabelecida
     }
 
-    public static String calculoMedia(int num1, int num2, int num3) {
-        return "O resultado da média vai ser de: " + (num1 + num2 + num3)/3; 
+    public static String calculoMedia(int num1, int num2, int num3) { //Cria função do tipo String, passando os parametros num1, num2, num3
+        return "O resultado da média vai ser de: " + (num1 + num2 + num3)/3; //Retorna o resultado somanddo e dividindo os parametros passados
     }
 }
