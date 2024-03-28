2487386c06dbadbaf6d92ed82fe65c3541ddb759
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-03-28 20:17:04 -0300

    ARRAYS

--- IndexJava/src/exercicioCursoExtra/arrays/mediaEscola.java
+++ IndexJava/src/exercicioCursoExtra/arrays/mediaEscola.java
@@ -7,11 +7,22 @@ public class mediaEscola {
     public static void main(String[] args) {
         
         List<Double> notas = new ArrayList<>(Arrays.asList(7.9, 8.0, 9.2, 6.0, 5.3));
-
+        exibeResultado(notas);
     }
 
     public static void exibeResultado(List<Double> listaNotas){
         int numAprovados = 0;
         int numReprovados = 0;
+
+        for (int i = 0; i < listaNotas.size(); i++) {
+            if(listaNotas.get(i) <= 7){
+                numReprovados += 1;
+            }else{
+                numAprovados += 1;
+            } 
+        }
+
+        System.out.println("Número de aprovados: " + numAprovados);
+        System.out.println("Número de reprovados: " + numReprovados);
     }
 }
