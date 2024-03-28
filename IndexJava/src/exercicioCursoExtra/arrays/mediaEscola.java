b12c5aff5c96059b4c46cd3207ed81d15d18a5f8
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-03-28 20:21:15 -0300

    comments

--- IndexJava/src/exercicioCursoExtra/arrays/mediaEscola.java
+++ IndexJava/src/exercicioCursoExtra/arrays/mediaEscola.java
@@ -1,28 +1,29 @@
 package exercicioCursoExtra.arrays;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class mediaEscola {
     public static void main(String[] args) {
         
-        List<Double> notas = new ArrayList<>(Arrays.asList(7.9, 8.0, 9.2, 6.0, 5.3));
-        exibeResultado(notas);
+        List<Double> notas = new ArrayList<>(Arrays.asList(7.9, 8.0, 9.2, 6.0, 5.3)); //Array com as notas
+        exibeResultado(notas); //Chama função e coloca a lista das notas como parametro
     }
 
-    public static void exibeResultado(List<Double> listaNotas){
-        int numAprovados = 0;
-        int numReprovados = 0;
+    public static void exibeResultado(List<Double> listaNotas){ //Cria a função colocando como paramentro uma lista "Imaginária"
+        int numAprovados = 0; //contador de aprovados
+        int numReprovados = 0; //contador de reprovados
 
-        for (int i = 0; i < listaNotas.size(); i++) {
-            if(listaNotas.get(i) <= 7){
-                numReprovados += 1;
+        for (int i = 0; i < listaNotas.size(); i++) { //Enquanto i for menor que tamanho da lista então...
+            if(listaNotas.get(i) <= 7){ //Se algum elemento da lista for menor ou igual a 7 então...
+                numReprovados += 1; //Adiciona + 1 ao contador de reprovados
             }else{
-                numAprovados += 1;
+                numAprovados += 1; //Adiciona + 1 ao contador de aprovados
             } 
         }
 
-        System.out.println("Número de aprovados: " + numAprovados);
-        System.out.println("Número de reprovados: " + numReprovados);
+        //Sysout fora do for para não repetir o contador, colocando apenas o seu ultimo resultado
+
+        System.out.println("Número de aprovados: " + numAprovados); //Resultado do contador
+        System.out.println("Número de reprovados: " + numReprovados); //Resultado do contador
     }
 }
